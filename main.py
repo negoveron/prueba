bf88e04c9ae575e712132f031149bbee0e04add0
Guille Veron <negoveron@gmail.com>
2024-08-14 21:50:30 -0300

    se sube main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+    
+app = FastAPI()
+
+
+
+# Endpoints de la API
+# @profile
+@app.get('/cantidad_filmaciones_mes/{mes}')
+async def cantidad_filmaciones_mes(mes: str):
+    try:
+         
+        return {f"108 películas fueron estrenadas en el mes de {mes.capitalize()}"}
+
+    except Exception as e:
+        return {"error": str(e)}            
+    
+
+@app.get('/cantidad_filmaciones_dia/{dia}')
+async def cantidad_filmaciones_dia(dia: str): 
+    try:  
+        
+        return {f" 25 películas fueron estrenadas en los días {dia.capitalize()}"}
+
+    except Exception as e:
+        return {"error": str(e)}  
